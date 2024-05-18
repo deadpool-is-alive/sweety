750a6933673d35fcf72b4f144d5787d3a8115418
Ansh Kumar <anshydv8@gmail.com>
2024-05-18 22:35:22 +0530

    Copied till project to repo clone
    It contain's prebuilt assets project and changes in gitignore to have saved files

---
+++ Source/UnrealLearningKit/MyActor.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{ 	
+	Super::BeginPlay();
+	StartLocation = GetActorLocation();
+	
+	
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	FVector CurrentLocation;
+	Super::Tick(DeltaTime);
+	CurrentLocation = GetActorLocation();
+	CurrentLocation += MovementSpeed * DeltaTime;
+	SetActorLocation(CurrentLocation);
+	DistanceMoved = FVector::Dist(StartLocation, CurrentLocation);
+
+	if(DistanceMoved >= MoveDistance)
+	{
+		FVector MoveDirection = MovementSpeed.GetSafeNormal();
+		StartLocation += MoveDistance*MoveDirection;
+		SetActorLocation(StartLocation);
+		float offset = FVector::Dist(StartLocation, GetActorLocation());
+		UE_LOG(LogTemp,Warning,TEXT("Log Printing %f"),offset);
+		MovementSpeed = -MovementSpeed;
+	}
+}
+
